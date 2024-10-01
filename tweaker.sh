e8698a38668676d061276c85510ca79743b8da82
unknowtherwise67 <98449530+unknowtherwise67@users.noreply.github.com>
2024-10-02 02:43:08 +0700

    Add files via upload

--- tweaker.sh
+++ tweaker.sh
@@ -16,6 +16,8 @@ write() {
 	echo "- $1 → $2"
 }
 
+# Variables
+
 # Sync Data
 sync
 
@@ -1036,6 +1038,10 @@ write /sys/class/kgsl/kgsl-3d0/devfreq/gpufreq/mali_ondemand/vsync_upthreshold 5
 write /sys/module/adreno_idler/parameters/adreno_idler_idleworkload 10000
 write /sys/class/simple_gpu_algorithm/parameters/simple_ramp_threshold 10000
 write /sys/class/kgsl/kgsl-3d0/throttling 0
+write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 0
+write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 0
+write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 0
+write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 0
 write /sys/module/adreno_idler/parameters/adreno_idler_active Y
 write /sys/devices/platform/13040000.mali/power_policy always_on
 write /sys/class/kgsl/kgsl-3d0/power_policy always_on
@@ -1052,62 +1058,6 @@ write /sys/devices/system/cpu/cpu8/online 1
 write /sys/devices/system/cpu/cpu9/online 1
 write /sys/devices/system/cpu/cpu10/online 1
 write /sys/devices/system/cpu/cpu11/online 1
-
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 1
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 2
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 3
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 4
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 5
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 6
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 7
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 8
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 9
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 10
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 11
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 12
-write /sys/class/kgsl/kgsl-3d0/min_pwrlevel 13
-
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 1
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 2
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 3
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 4
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 5
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 6
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 7
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 8
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 9
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 10
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 11
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 12
-write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 13
-
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 1
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 2
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 3
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 4
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 6
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 7
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 8
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 9
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 10
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 11
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 12
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 13
-
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 1
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 2
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 3
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 4
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 5
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 6
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 7
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 8
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 9
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 10
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 11
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 12
-write /sys/class/kgsl/kgsl-3d0/thermal_pwrlevel 13
 
 for queue in /sys/*/*/queue
 do
