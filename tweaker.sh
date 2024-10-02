d202fa82acda3f935408709c6feabbafa44b65ce
unknowtherwise67 <98449530+unknowtherwise67@users.noreply.github.com>
2024-10-02 13:30:00 +0700

    Add files via upload

--- tweaker.sh
+++ tweaker.sh
@@ -1077,15 +1077,6 @@ done
 
 for queue in /sys/*/*/queue
 do
-	write "$queue/iostats" 1
-	write "$queue/add_random" 1
-	write "$queue/rotational" 1
-	write "$queue/rq_affinity" 2
-	write "$queue/nomerges" 2
-done
-
-for queue in /sys/*/*/queue
-do
 	write "$queue/iosched/front_merges" 1
 	write "$queue/iosched/back_seek_penalty" 10
 	write "$queue/iosched/slice_async_rq" 10
