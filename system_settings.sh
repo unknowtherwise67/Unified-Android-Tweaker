7c98f8fbed3f9ca40e1191152f1d745aa96bd677
unknowtherwise67 <98449530+unknowtherwise67@users.noreply.github.com>
2025-08-09 23:08:32 +0700

    Add files via upload

--- system_settings.sh
+++ system_settings.sh
@@ -30,18 +30,30 @@ perfmgr="/proc/perfmgr/"
 # Sync Data
 sync
 
-# Settings
-settings put global development_settings_enabled 0
-wm disable-blur 0
-settings put global disable_window_blurs 0
-settings put system screen_brightness_mode 0
+# System Screen Refresh Rate
 settings put system peak_refresh_rate 0
 settings put system min_refresh_rate 0
 settings put system low_power_refresh_rate 0
-settings put global adb_enabled 0
+
+# System Blurs Effects
+wm disable-blur 0
+settings put global disable_window_blurs 0
+
+# System Animations
 settings put global window_animation_scale 1.00
 settings put global transition_animation_scale 1.00
 settings put global animator_duration_scale 1.00
+
+# System Settings
+settings put system screen_brightness_mode 0
+settings put system intelligent_sleep_mode 0
+settings put global app_restriction_enabled false
+settings put system status_bar_show_battery_percent 2
+settings put global dynamic_power_savings_disable_threshold 10
+settings put global accessibility_reduce_transparency 0
+settings put global enhanced_processing 0
+settings put global development_settings_enabled 0
+settings put global adb_enabled 0
 settings put global wifi_scan_always_enabled 0
 settings put global ble_scan_always_enabled 0
 settings put global adaptive_battery_management_enabled 0
@@ -49,31 +61,28 @@ settings put global tether_offload_disabled 0
 settings put global wifi_power_save 0
 settings put global network_scoring_ui_enabled 0
 settings put global cached_apps_freezer disabled 0
-settings put secure adaptive_sleep 0
+settings put global sem_enhanced_cpu_responsiveness 0
 settings put global dynamic_power_savings_enabled 0
 settings put global automatic_power_save_mode 0
 settings put global app_standby_enabled 0
-settings put secure screensaver_activate_on_sleep 0
-settings put secure screensaver_enabled 0
 settings put global network_recommendations_enabled 0
 settings put secure bluetooth_a2dp_bt_uhq_state 1
 settings put secure bluetooh_a2dp_uhqa_support 1
-settings put system tube_amp_effect 1
-settings put system k2hd_effect 1
 settings put secure tap_duration_threshold 0.0
 settings put secure touch_blocking_period 0.0
+settings put secure screensaver_activate_on_sleep 0
+settings put secure screensaver_enabled 0
+settings put secure adaptive_sleep 0
 settings put global settings_enable_monitor_phantom_procs disable
-setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs disable
-dumpsys deviceidle disable
-
-# Thermal/HotPlug/Processor Controls
-stop thermal
-setprop ctl.stop mpdecision;stop mpdecision
-write /sys/module/msm_thermal/core_control/enabled 0
-write /sys/module/msm_thermal/vdd_restriction/enabled 0
 
 # Others
 write /sys/block/zram0/initstate 1
 write /proc/sys/kernel/perf_cpu_time_max_percent 1
 write /proc/sys/vm/page-cluster 100
 write /proc/sys/vm/drop_caches 0
+setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs disable
+dumpsys deviceidle disable
+stop thermal
+setprop ctl.stop mpdecision;stop mpdecision
+write /sys/module/msm_thermal/core_control/enabled 0
+write /sys/module/msm_thermal/vdd_restriction/enabled 0
