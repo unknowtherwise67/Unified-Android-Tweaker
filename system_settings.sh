b9453d63698226935336efbb632ca02bc6665902
unknowtherwise67 <98449530+unknowtherwise67@users.noreply.github.com>
2026-03-30 21:24:47 +0700

    Add files via upload

--- system_settings.sh
+++ system_settings.sh
@@ -48,7 +48,6 @@ settings put global animator_duration_scale 1.00
 # System Settings
 settings put system screen_brightness_mode 0
 settings put system intelligent_sleep_mode 0
-settings put global app_restriction_enabled false
 settings put global dynamic_power_savings_disable_threshold 10
 settings put global accessibility_reduce_transparency 0
 settings put global enhanced_processing 1
@@ -56,16 +55,20 @@ settings put global development_settings_enabled 0
 settings put global adb_enabled 0
 settings put global wifi_scan_always_enabled 1
 settings put global ble_scan_always_enabled 1
+settings put global bluetooth_a2dp_offload_disabled 1
+settings put global sem_enhanced_cpu_responsiveness 1
 settings put global adaptive_battery_management_enabled 0
 settings put global tether_offload_disabled 0
 settings put global wifi_power_save 0
+settings put global network_restricted_mode 0
 settings put global network_scoring_ui_enabled 0
 settings put global cached_apps_freezer disabled 0
-settings put global sem_enhanced_cpu_responsiveness 0
 settings put global dynamic_power_savings_enabled 0
 settings put global automatic_power_save_mode 0
 settings put global app_standby_enabled 0
 settings put global network_recommendations_enabled 0
+settings put global app_restriction_enabled false
+settings put global settings_enable_monitor_phantom_procs disable
 settings put secure bluetooth_a2dp_bt_uhq_state 1
 settings put secure bluetooh_a2dp_uhqa_support 1
 settings put secure tap_duration_threshold 0.0
@@ -73,10 +76,8 @@ settings put secure touch_blocking_period 0.0
 settings put secure screensaver_activate_on_sleep 0
 settings put secure screensaver_enabled 0
 settings put secure adaptive_sleep 0
-settings put global settings_enable_monitor_phantom_procs disable
 
 # Others
-setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs disable
 dumpsys deviceidle disable
 write /sys/module/msm_thermal/core_control/enabled 0
 write /sys/module/msm_thermal/vdd_restriction/enabled 0
